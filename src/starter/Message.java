6ef081e1bd87139d62e1c529e29f74a47dff786b
labfive33 <labfive33@gmail.com>
2023-03-08 21:12:37 +0530

    Initiate local repository pull uploads to remote repository

---
+++ src/starter/Message.java
@@ -0,0 +1,19 @@
+/**
+ * Java sample code 
+ */
+package starter;
+
+public class Message {
+	
+	public static void dispMessage() {
+		
+		System.out.println("This is a static reference");
+	}
+
+	public static void main(String[] args) {
+		
+		dispMessage(); 
+
+	}
+
+}
