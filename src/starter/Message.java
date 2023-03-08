10cbf05f0e618d52317b3b2aee0b3a37b6e68e3e
labfive33 <labfive33@gmail.com>
2023-03-08 21:42:17 +0530

    Starter java code

--- src/starter/Message.java
+++ src/starter/Message.java
@@ -10,10 +10,18 @@ public class Message {
 		System.out.println("This is a static reference");
 	}
 
+	public void dispAge() {
+		
+		int age = 25; 
+		System.out.println("This is a non-static reference" +age);
+		
+	}
+	
 	public static void main(String[] args) {
 		
 		dispMessage(); 
-
+		Message obj = new Message(); 
+		obj.dispAge();
 	}
 
 }
